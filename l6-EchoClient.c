d0e684158a478be19fbce93fffc286810cd57984
questionyu <questionyu@outlook.com>
2017-04-20 19:03:28 +0800

    Lab6

--- l6-EchoClient.c
+++ l6-EchoClient.c
@@ -15,44 +15,50 @@ int main(int argc, char *argv[]) {
 	unsigned int fromSize; /* In-out of address size for recvfrom() */
 	char *servIP; /* IP address of server */
 	char *echoString; /* String to send to echo server */
-	char echoBuffer[ECHOMAX + 1]; /* Buffer for receiving echoed string */\
-    int echoStringLen; /* Length of string to echo */
+	char echoBuffer[ECHOMAX + 1]; /* Buffer for receiving echoed string */
+	size_t echoStringLen; /* Length of string to echo */
 	int respStringLen; /* Length of received response */
 
-	if ((argc < 3) || (argc > 4)) { /* Test for correct number ofarguments */
-		printf("Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n", argv[0]);
+	if (argc < 3) { /* Test for correct number of arguments */
+		printf("Usage: %s <Server IP> <Echo Word>\n", argv[0]);
 		exit(1);
 	}
+
 	servIP = argv[1]; /* First arg: server IP address (dotted quad) */
-	echoString = argv[2]; /* Second arg: string to echo */
-	if ((echoStringLen = strlen(echoString)) > ECHOMAX) /* Check input length */
-		printf("Echo word too long.\n");
-	if (argc == 4)
-		echoServPort = atoi(argv[3]); /* Use given port, if any */
-	else
-		echoServPort = 7; /* 7 is the well-known port for echo service */
+	echoServPort = 56325;
+
 	/* Create a datagram/UDP socket */
 	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
 		printf("socket() failed.\n");
 	/* Construct the server address structure */
-	memset(&echoServAddr, 0, sizeof(echoServAddr)); /*Zero out structure*/
+	memset(&echoServAddr, 0, sizeof(echoServAddr));/*Zero out structure*/
 	echoServAddr.sin_family = AF_INET; /* Internet addr family */
-	echoServAddr.sin_addr.s_addr = inet_addr(servIP); /*Server IP address*/
+	echoServAddr.sin_addr.s_addr = inet_addr(servIP);/*Server IP address*/
 	echoServAddr.sin_port = htons(echoServPort); /* Server port */
-	/* Send the string to the server */
-	if ((sendto(sock, echoString, echoStringLen, 0, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr))) != echoStringLen)
-		printf("sendto() sent a different number of bytes than expected.\n");
-	/* Recv a response */
-	fromSize = sizeof(fromAddr);
-	if ((respStringLen = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *) &fromAddr, &fromSize)) != echoStringLen)
-		printf("recvfrom() failed\n");
-	if (echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
-		printf("Error: received a packet from unknown source.\n");
-		exit(1);
+
+	for (int i = 2; i < argc; i++) {
+		echoString = argv[i]; /* Second arg: string to echo */
+		if ((echoStringLen = strlen(echoString)) > ECHOMAX) /* Check input length */
+			printf("Echo word too long.\n");
+		/* Send the string to the server */
+		if (sendto(sock, echoString, echoStringLen, 0, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) != echoStringLen)
+			printf("sendto() sent a different number of bytes than expected.\n");
+		printf("sending data to '%s'\n", servIP);
 	}
-	/* null-terminate the received data */
-	echoBuffer[respStringLen] = '\0';
-	printf("Received: %s\n", echoBuffer); /*Print the echoed message*/
+
+//	/* Recv a response */
+//	fromSize = sizeof(fromAddr);
+//	if ((respStringLen = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *) &fromAddr, &fromSize)) != echoStringLen)
+//		printf("recvfrom() failed\n");
+//
+//	if (echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
+//		printf("Error: received a packet from unknown source.\n");
+//		exit(1);
+//	}
+//	/* null-terminate the received data */
+//	echoBuffer[respStringLen] = '\0';
+//	printf("Received: %s\n", echoBuffer);/*Print the echoed message*/
 	close(sock);
-	exit(0);
+
+	return 0;
 }
--- l6-EchoServer.c
+++ l6-EchoServer.c
@@ -1,9 +1,9 @@
 #include <stdio.h> /* for printf() and fprintf() */
-#include <sys/socket.h> /* for socket(), bind(), sendto() and recvfrom() */
+//#include <sys/socket.h> /* for socket(), bind(), sendto() and recvfrom() */
 #include <arpa/inet.h> /* for sockaddr_in and inet_ntoa() */
-#include <stdlib.h> /* for atoi() and exit() */
+//#include <stdlib.h> /* for atoi() and exit() */
 #include <string.h> /* for memset() */
-#include <unistd.h> /* for close() */
+//#include <unistd.h> /* for close() */
 
 #define ECHOMAX 255 /* Longest string to echo */
 
@@ -14,14 +14,9 @@ int main(int argc, char *argv[]) {
 	unsigned int cliAddrLen; /* Length of client address */
 	char echoBuffer[ECHOMAX]; /* Buffer for echo string */
 	unsigned short echoServPort; /* Server port */
-	int recvMsgSize; /* Size of received message */
+	ssize_t recvMsgSize; /* Size of received message */
 
-	if (argc != 2) {
-		printf("Usage: %s <UDP SERVER PORT>\n", argv[0]);
-		exit(1);
-	}
-
-	echoServPort = atoi(argv[1]); /* First arg: local port */
+	echoServPort = 56325;
 	/* Create socket for sending/receiving datagrams */
 	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
 		printf("socket() failed.\n");
@@ -29,20 +24,23 @@ int main(int argc, char *argv[]) {
 	memset(&echoServAddr, 0, sizeof(echoServAddr));
 	echoServAddr.sin_family = AF_INET;
 	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	echoServAddr.sin_port = htons(echoServPort);
+	echoServAddr.sin_port =htons(echoServPort);
 	/* Bind to the local address */
 	if ((bind(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr))) < 0)
 		printf("bind() failed.\n");
+
 	for (;;) { /* Run forever */
 		/* Set the size of the in-out parameter */
 		cliAddrLen = sizeof(echoClntAddr);
 		/* Block until receive message from a client */
-		if ((recvMsgSize = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *) &echoClntAddr, &cliAddrLen)) < 0)
+		if ((recvMsgSize = recvfrom(sock, echoBuffer, ECHOMAX, 0,(struct sockaddr *) &echoClntAddr, &cliAddrLen)) < 0)
 			printf("recvfrom() failed.\n");
-		printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
-		/* Send received datagram back to the client */
-		if ((sendto(sock, echoBuffer, recvMsgSize, 0, (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr))) != recvMsgSize)
-			printf("sendto() sent a different number of bytes than expected.\n");
+		echoBuffer[recvMsgSize] = '\0';
+		printf("from %s:UDP%d : %s\n",inet_ntoa(echoClntAddr.sin_addr), echoClntAddr.sin_port, echoBuffer);
+//		/* Send received datagram back to the client */
+//		if ((sendto(sock, echoBuffer, recvMsgSize, 0, (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr))) != recvMsgSize)
+//			printf("sendto() sent a different number of bytes than expected.\n");
 	}
+
 	return 0;
 }
