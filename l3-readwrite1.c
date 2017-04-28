af67627949d027d1394608db96b651af497c062c
questionyu <questionyu@outlook.com>
2017-04-28 10:47:36 +0800

    Optimize codes.

--- l3-readwrite1.c
+++ l3-readwrite1.c
@@ -6,7 +6,7 @@ int main(void) {
 	char quit = '.';
 	char buf[10];
 	int fd;
-	if ((fd = open("out.out", O_RDWR | O_CREAT, 0)) == -1)
+	if ((fd = open("out.out", O_RDWR | O_CREAT)) == -1)
 		printf("Error in opening\n");
 	while (buf[0] != quit) {
 		read(0, buf, 1);
