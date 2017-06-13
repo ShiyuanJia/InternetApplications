f7c3b6c897deeca7cadc605a9e66e23a012212b8
lixinlong111 <1376790189@qq.com>
2017-06-13 22:12:58 +0800

    修正了链表指针的错误用法

--- dhcp-client.c
+++ dhcp-client.c
@@ -1,7 +1,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <linux/if.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
 #include <net/if.h>
 #include <unistd.h>
 #include <stdio.h>
